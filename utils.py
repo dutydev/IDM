87395657cd82c361d5b8d875ddc40ee5a894f225
LordRalInc <46781434+LordRalInc@users.noreply.github.com>
2020-02-21 15:34:01 +0300

    Add files via upload

---
+++ utils.py
@@ -0,0 +1,57 @@
+from .objects import DB
+from vkapi import VkApi, VkApiResponseException
+import json
+from flask import render_template
+import typing
+import time
+
+def get_all_history_gen(vk: VkApi, chat_id: int) -> typing.Generator[dict, None, None]:
+    __offset = 0
+    chat = vk("messages.getHistory", count=1, peer_id=chat_id, offset=__offset)
+    while __offset < chat['count']:
+        chat = {}
+
+        try:
+            
+            chat = vk("messages.getHistory", count=200,
+                        peer_id=chat_id, offset=__offset)
+            time.sleep(1)        
+        except:
+            time.sleep(3)            
+            continue
+
+        __offset += 200 
+        for item in chat['items']:
+            yield item
+
+def get_msg_id(vk: VkApi, chat_id: int, local_id: int) -> typing.Union[int, None]:
+    try:
+        data = vk("messages.getByConversationMessageId", peer_id=chat_id, conversation_message_ids=local_id)
+        return data['items'][0]['id']
+    except:
+        return None
+
+def get_msg(vk: VkApi, chat_id: int, local_id: int)-> typing.Union[dict, None]:
+    try:
+        data = vk("messages.getByConversationMessageId", peer_id=chat_id, conversation_message_ids=local_id)
+        if len(data['items']) != 0:
+            return data['items'][0]
+        return None
+    except:
+        return None
+
+
+def get_msg_ids(vk: VkApi, chat_id: int, local_ids: list) -> typing.Generator[int, None, list]:
+    try:
+        local_ids = [str(li) for li in local_ids]
+        data = vk("messages.getByConversationMessageId", peer_id=chat_id, conversation_message_ids=",".join(local_ids))
+        for item in data['items']:
+            yield item['id']
+    except:
+        return []
+
+def edit_message(api: VkApi, chat_id: int, msg_id: int, **kwargs) -> int:
+    return api("messages.edit", peer_id=chat_id, message_id=msg_id, **kwargs)
+
+def new_message(api: VkApi, chat_id: int, **kwargs) -> int:
+    return api("messages.send", random_id=0, peer_id=chat_id, **kwargs)
