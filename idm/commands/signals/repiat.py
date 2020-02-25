9b52bec304b278ce51c182cccbb9d3591156b4a2
LordRalInc <46781434+LordRalInc@users.noreply.github.com>
2020-02-26 01:47:47 +0300

    hotfix

--- idm/commands/signals/repiat.py
+++ idm/commands/signals/repiat.py
@@ -3,6 +3,8 @@ from ...utils import new_message
 
 @dp.signal_event_handle('повтори')
 def repiat(event: SignalEvent) -> str:
+    if event.msg['from_id'] not in event.db.trusted_users:
+        return "ok"
     new_message(event.api, event.chat.peer_id, message=event.payload,
         attachments=",".join(event.attachments))
 
