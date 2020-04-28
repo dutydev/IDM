0fe2a6765b0c6bccff944ed9997e1cb08cea5a89
LordRalInc <46781434+LordRalInc@users.noreply.github.com>
2020-04-28 22:09:09 +0300

    h

--- dutymanager/web/forms.py
+++ dutymanager/web/forms.py
@@ -1,46 +1,50 @@
 from aiohttp import web
 from jinja2 import Environment, FileSystemLoader
 from dutymanager.units import const
-from module.utils import DotDict
 
 enviroment = Environment(
     loader=FileSystemLoader(const.TEMPLATES_PATH)
 )
 
-class LoginForm:
 
+class LoginForm:
     template_name = 'forms/login.html'
 
     login: str
     password: str
 
-    errors: DotDict = DotDict(
-        form=[],
-        login=[],
-        password=[]
-    )
+    errors: dict
 
-    def __init__(self, post: 'MultiDictProxy[Union[str, bytes, FileField]]' = {}):
+    def __init__(self, post: 'MultiDictProxy[Union[str, bytes, FileField]]' = None):
+        print(post)
         self.login = post.get('login', None) if post else None
         self.password = post.get('password', None) if post else None
+        self.errors = {
+            "form": [],
+            "login": [],
+            "password": []
+        }
 
     def __html__(self):
         template = enviroment.get_or_select_template(self.template_name)
         return template.render(
-            context={
+            **{
                 "login": self.login or "",
                 "password": "",
-                "errors": self.errors
+                "form_errors": self.errors
             }
         )
 
     def validate(self):
         is_valide = True
-        if not self.login:
-            self.errors.login.append("Пустой логин")
-            is_valide = False
-        if not self.login:
-            self.errors.login.append("Пустой пароль")
-            is_valide = False
-        return is_valide
 
+        if not self.login:
+            self.errors['login'].append("Пустой логин")
+            is_valide = False
+
+        if not self.password:
+            self.errors['password'].append("Пустой пароль")
+            is_valide = False
+
+
+        return is_valide
--- dutymanager/web/viewers.py
+++ dutymanager/web/viewers.py
@@ -1,10 +1,12 @@
 from aiohttp import web
 from aiohttp_jinja2 import render_template
 from dutymanager.web import forms
+from module.utils import logger
 
 
 async def index(request: web.Request) -> web.Response:
     assert isinstance(request, web.Request)
+    logger.info(f'{request.method} -> {request.path}')
     return render_template(
         'dutymanager/index.html',
         request,
@@ -16,11 +18,17 @@ async def index(request: web.Request) -> web.Response:
 
 async def login(request: web.Request) -> web.Response:
     assert isinstance(request, web.Request)
+    logger.info(f'{request.method} -> {request.path}')
     if request.method == 'POST':
         post_data = await request.post()
         login_form = forms.LoginForm(post_data)
+        if login_form.validate():
+            logger.info('Форма валидна')
+            raise web.HTTPFound('/')
     else:
         login_form = forms.LoginForm()
+
+    logger.info(f'{(login_form.errors, login_form.login, login_form.password)}')
 
     return render_template(
         'dutymanager/login.html',
