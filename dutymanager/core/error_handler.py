55b435868faa88db3a55046f71dafdbd720d53ad
Zyoma Podushkin <danik3399@gmail.com>
2020-05-20 21:14:17 -0500

    Added error_message in the error_handler

--- dutymanager/core/error_handler.py
+++ dutymanager/core/error_handler.py
@@ -1,10 +1,12 @@
+import re
+
 from dutymanager.units.vk_script import msg_edit
 from dutymanager.db.methods import AsyncDatabase
 from dutymanager.units.utils import send_msg
 from module import Blueprint, VKError
 from traceback import format_exc
 from module.utils import logger
-from re import findall
+from typing import Optional
 
 bot = Blueprint(name="Error Handler")
 db = AsyncDatabase.get_current()
@@ -16,25 +18,16 @@ MESSAGE = """❗ Произошла ошибка.
 
 @bot.error_handler(KeyError)
 async def key_error(e: KeyError, event: dict):
-    if findall("[0-9a-zA-Z]{8}", str(e)):
+    if re.findall("[0-9a-zA-Z]{8}", str(e)):
         logger.error(
             "Чат {} всё еще не был связан.",
             event["object"]["chat"]
         )
         return {"response": "error", "error_code": 4}
-    logger.error(format_exc(5))
 
 
 @bot.error_handler(VKError)
-async def swear(e: VKError, event: dict):
-    """
-    Абстрактная функция, которая ловит ошибки от VK.
-    Сделана для того, чтобы не оборачивать каждый
-    хэндлер в try/except блоки.
-    :param e: Класс ошибки (VKError)
-    :param event: Пришедший эвент/сигнал
-    """
-    logger.error(format_exc(5))
+async def swear(e: VKError, event: dict) -> Optional[dict]:
     if event["method"] in ("ping", "banExpired"):
         return
     uid = event["object"]["chat"]
@@ -53,4 +46,8 @@ async def swear(e: VKError, event: dict):
             event["message"]["conversation_message_id"]
         )
     await send_msg(db.chats(uid), message)
-    return {"response": "vk_error", "error_code": e.error_code}
+    return {
+        "response": "vk_error",
+        "error_message": e.error_description,
+        "error_code": e.error_code
+    }
