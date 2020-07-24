8a818e7f233fca645d45cb2f01b5dd305a819d43
Josek <Elchin751@gmail.com>
2020-07-24 18:50:04 +0300

    Убрал сообщение, уже неактуально

--- idm/commands/ping.py
+++ idm/commands/ping.py
@@ -5,9 +5,4 @@ from microvk import VkApi
 
 @dp.event_handle('ping')
 def ping(event: Event) -> str:
-    if event.db.informed == False:
-        event.api('execute', code = 'return API.messages.send({peer_id:"-195759899",message:"По поводу ошибки' +
-        ' таймаута - смотри пост в этой группе. Если есть идеи, как на одном потоке закрыть соединение - поделись",random_id:0});')
-        event.db.informed = True
-        event.db.save()
     return "ok"
