0c16db1dc03155ee32ef8aac09237dba4327731f
Josek <Elchin751@gmail.com>
2020-11-04 14:29:54 +0300

    1.3.3 beta rev.2
    -- микрофикс

--- idm/callback_signals/ban_get_reason.py
+++ idm/callback_signals/ban_get_reason.py
@@ -1,8 +1,13 @@
 from idm.objects import dp, Event
 from idm.api_utils import get_msg_id
 
+
 @dp.event_register('banGetReason')
 def ban_get_reason(event: Event) -> str:
-    event.api.msg_op(1, event.chat.peer_id, event.obj['message'], 
-        reply_to=get_msg_id(event.api, event.chat.peer_id, event.obj['local_id']))   
+    reply = {}
+    if event.obj['local_id'] != 0:
+        reply['reply_to'] = get_msg_id(
+            event.api, event.chat.peer_id, event.obj['local_id']
+        )
+    event.api.msg_op(1, event.chat.peer_id, event.obj['message'], **reply)
     return "ok"
