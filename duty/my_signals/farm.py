b66f2336ce7f1d786c278596eabd97225d15bbbb
Elchinchel <Elchin751@gmail.com>
2022-02-16 12:34:38 +0300

    1.5.3 beta rev.4
    -- фикс фермы

--- duty/my_signals/farm.py
+++ duty/my_signals/farm.py
@@ -1,6 +1,7 @@
 from duty.objects import dp, MySignalEvent
 from time import sleep
 
+
 farm_data = {
     "owner_id": -174105461,
     "post_id": 6713149
@@ -10,10 +11,14 @@ farm_data = {
 @dp.longpoll_event_register('ферма')
 @dp.my_signal_event_register('ферма')
 def farming(event: MySignalEvent) -> str:
-    comment_id = event.api('wall.createComment', message='ферма', **farm_data)['comment_id']
-    event.msg_op(2, '⏱ Комментарий оставлен')
+    comment_id = event.api('wall.createComment',
+                           message='ферма', **farm_data)['comment_id']
+    event.edit('⏱ Комментарий оставлен...')
     sleep(2)
-    reply_text = event.api('wall.getComments', **farm_data,
-                           comment_id=comment_id)['items'][0]['text']
-    event.msg_op(2, reply_text)
+    replies = event.api('wall.getComments',
+                        **farm_data, comment_id=comment_id)['items']
+    if replies:
+        event.edit(replies[0]['text'])
+    else:
+        event.edit('😐 Ирис не ответил.')
     return "ok"
