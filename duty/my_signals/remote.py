ea36be21ca039197cc2f36b16c3bbe8904b8a1c2
Obnovlator3000 <github@lisi4ka.ru>
2022-02-23 13:19:20 +0300

    Чуток переделал для проверки уповтори по секрет коду, а не по сессии Наладил ЦОД И вообще я молодец

--- duty/my_signals/remote.py
+++ duty/my_signals/remote.py
@@ -1,11 +1,16 @@
+import time
+
 from duty.objects import dp, MySignalEvent, db
 from duty.utils import find_mention_by_event, get_plural, cmid_key
 from typing import Union
 import requests
+from microvk import VkApi, VkApiResponseException
+from flask import  request
 
 session = None
 
-DC = 'https://IrcaDC.pythonanywhere.com'
+DC = 'https://api.lisi4ka.ru/'
+group_dc = -201667822
 
 errors = {
     4: ('❗ На удаленном сервере отсутствует данный чат\n' +
@@ -23,63 +28,78 @@ def set_session(ses: str) -> str:
     return ses
 
 
+@dp.longpoll_event_register('+цод')
+@dp.my_signal_event_register('+цод')
+def reg_dc(event: MySignalEvent):
+    protocol = 'https' if 'pythonanywhere' in request.host else 'http'
+    VkApi(db.access_token).msg_op(1, group_dc, f'+cod {db.secret} {protocol}://{request.host}/')
+    time.sleep(0.5)
+    event.msg_op(2, f'Запрос отправлен. Иди проверяй.')
+    return "ok"
+
+
+@dp.longpoll_event_register('чек')
+@dp.my_signal_event_register('чек')
+def check(event: MySignalEvent):
+    uid = find_mention_by_event(event)
+    resp = requests.get(DC + f'reg/{uid}', timeout=10)
+    if resp.status_code != 200:
+        event.msg_op(1, '❗ Проблемы с центром обработки данных\n' +
+        'Напиши [id230192963|этому челику], если он еще живой',
+        disable_mentions = 1)
+        return "ok"
+
+    r = resp.json()
+    if r['status'] == 'error':
+        msg = r['error']
+        event.msg_op(2, msg)
+        return "ok"
+    else:
+        msg = f'[id{uid}|Пользователь] {"" if r["is_registered"] == 1 else "не"} зарегестрирован.'
+        event.msg_op(2, msg)
+        return "ok"
+
+
 @dp.longpoll_event_register('цод')
 @dp.my_signal_event_register('цод')
 def dc(event: MySignalEvent):
-    resp = requests.post(DC, json={
-        'method': 'info',
-        'user_id': str(event.db.owner_id),
-        'session': session
-    }, timeout=5)
+    print('cod')
+    resp = requests.get(DC + 'stat', timeout=10)
     if resp.status_code != 200:
-        if resp.status_code == 403:
-            return "ok"
         event.msg_op(1, '❗ Проблемы с центром обработки данных\n' +
-                     'Напиши [id332619272|этому челику], если он еще живой',
+                     'Напиши [id230192963|этому челику], если он еще живой',
                      disable_mentions=1)
         return "ok"
-    users = resp.json()['users']
-    event.msg_op(2, f'Зарегистрировано {users} пользовател{get_plural(users, "ь", "я", "ей")}')  # noqa
-    return "ok"
+    users = resp.json()['count']
+    event.msg_op(2, f'Зарегистрировано {users} пользовател{get_plural(users, "ь", "я", "ей")}')
+    return 'ok'
 
 
 @dp.longpoll_event_register('унапиши', 'у')
 @dp.my_signal_event_register('унапиши', 'у')
 def remote_control(event: MySignalEvent) -> Union[str, dict]:
-    if db.dc_auth is False:
-        event.msg_op(2, '❗ Для использования этой команды необходимо ' +
-                     'разрешить авторизацию в ЦОД по токену (на сайте)')
-        return "ok"
-
     uid = find_mention_by_event(event)
     if uid is None:
         event.msg_op(2, '❗ Необходимо указать пользователя')
         return "ok"
 
-    resp = requests.post(DC, json={
-        'method': 'remote_control',
-        'remote_user': str(uid),
-        'user_id': str(event.db.owner_id),
-        'session': session,
-        'data': {
-            'chat': event.chat.iris_id,
-            'local_id': event.msg[cmid_key]
-        }
-    }, timeout=5)
+    resp = requests.post(DC + 'repeat', json={
+        'user_id': uid,
+        'owner_id': event.db.owner_id,
+        'chat': event.chat.iris_id,
+        'local_id': event.msg[cmid_key],
+        'secret': db.secret
+    }, timeout=10)
     if resp.status_code != 200:
         event.msg_op(1, '❗ Проблемы с центром обработки данных\n' +
-                     'Напиши [id332619272|этому челику], если он еще живой',
+                     'Напиши [id230192963|этому челику], если он еще живой',
                      disable_mentions=1)
         return "ok"
 
-    resp = resp.json()
+    r = resp.json()
 
-    if 'error' in resp:
-        code = resp['error']
-        if code == 5:
-            msg = f"❗ Ошибка VK #{resp['code']}: {resp['msg']}"
-        else:
-            msg = errors.get(code, '❗ Неизвестный код ошибки')
+    if r['status'] == 'error':
+        msg = r['error']
         event.msg_op(2, msg)
         return "ok"
 
